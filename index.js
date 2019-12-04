6cc9c331a702b407e99c5df0ef14479f4ac58d52
Gabriel Frink <frinkdotdesign@gmail.com>
2019-12-04 08:31:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// write your code below!
+learn test
+//Questions 1
+function happyHolidays(){
+  return "Happy holidays!";
+}
+
+//Question 2
+var name = "you";
+  
+function happyHolidaysTo(name){
+  var name = "you";
+ return 'Happy holidays, ${name}!';
+}
+//Question 3
+var holiday = "holiday";
+var name = "you";
+function happyHolidayTo(Hanukuh, you){
+  return "Hqpppy ${holiday}, ${name}!";
+}
+//Question 4
+function holidayCountdown(days, holiday){
+  var holiday = "Mother's Day";
+  var days = "20 days";
+  return "It's ${days} days until ${holiday}!";  
+  
+  
+}
